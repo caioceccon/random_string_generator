ca44fac8b56d1b126cdebac5b2a6098c2661c0f0
Caio Ceccon <contact@caioceccon.com>
2017-08-31 00:36:11 +0200

    Add missing description for the project.

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule RandomStringGenerator.Mixfile do
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
+      description: description(),
       package: package(),
       source_url: "https://github.com/caioceccon/random_string_generator/",
     ]
@@ -27,6 +28,12 @@ defmodule RandomStringGenerator.Mixfile do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
+  end
+
+  defp description do
+    """
+    Generate a random string based on a given string pattern.
+    """
   end
 
   defp package do
