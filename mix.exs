977c232d61b5fdf95500bd60a18d4fc8312cf430
Caio Ceccon <contact@caioceccon.com>
2017-08-31 00:31:49 +0200

    Add package metadata for hex.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule RandomStringGenerator.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      source_url: "https://github.com/caioceccon/random_string_generator/",
     ]
   end
 
@@ -24,6 +26,15 @@ defmodule RandomStringGenerator.Mixfile do
       {:ex_doc, "~> 0.16", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "test", "mix.exs", "README*", "LICENSE"],
+      maintainers: ["Caio Ceccon"],
+      licenses: ["GNU General Public License v3.0"],
+      links: %{"GitHub" => "https://github.com/caioceccon/random_string_generator"}
     ]
   end
 end
