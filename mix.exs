676f3c11d1d6e280ad27ef2d3cb25617c6ca505a
Caio Ceccon <contact@caioceccon.com>
2018-08-08 20:39:09 +0200

    Add excoveralls for test env

--- mix.exs
+++ mix.exs
@@ -9,6 +9,13 @@ defmodule RandomStringGenerator.Mixfile do
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       description: description(),
       package: package(),
       docs: docs()
@@ -25,7 +32,8 @@ defmodule RandomStringGenerator.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.9.1", only: :test, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
